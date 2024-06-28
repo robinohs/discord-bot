21aebe7dbec0b64484ec19b477de68bb87c44564
Robin Ohs <me@robinohs.dev>
2024-06-28 19:35:26 +0200

    feat: add basic connectivity and message handler.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use anyhow::Result;
+use dotenvy::dotenv;
+use serenity::{
+    all::{Context, EventHandler, GatewayIntents, Message},
+    async_trait, Client,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!test" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // load environment variables from .env file
+    dotenv().expect(".env file not found");
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN")?;
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    // Create a new instance of the Client, logging in as a bot.
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    // Start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+
+    Ok(())
 }
